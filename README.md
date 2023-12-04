3fc392d01cd453200117d1e7d844986c50d62614
nanaaoki <aoki.nana@gmail.com>
2023-12-03 22:37:31 -0500

    completed workshop

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Unit2.PartyPlanner
+
+Build your website according to this user story:
+
+A user enters the website and finds a list of the names, dates, times, locations, and descriptions of all the parties that are happening.
+Next to each party in the list is a delete button. The user clicks the delete button for one of the parties. That party is then removed from the list.
+There is also a form that allows the user to enter information about a new party that they want to schedule. After filling out the form and submitting it, the user observes their party added to the list of parties.
+
+Read the API documentation. You will be working with the /events endpoint.
+.. https://fsa-crud-2aa9294fe819.herokuapp.com/api/
+
+HINTS:
+1) Which components can be created directly in the HTML? Which components need to be created in JavaScript?
+2) Can you render mock data to the page?
+3) Can you render real data to the page?
+4) Are you able to fetch an array of all the parties from the API?
+5) Is state correctly updated to match the data from the API?
+6) Are you passing the correct arguments to fetch?
+7) Does the API return an error? If so, what is the error message?
+8) Is there an event listener on the form? Does it correctly add a new party to the list of parties?
+9) Is there an event listener attached to each delete button? Does it correctly remove a party from the list of parties?
+
+
+
---
+++ party.js
@@ -0,0 +1,109 @@
+// API_URL has the data we need and where new data will be saved
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-pt-sf-b-nana/events`;
+
+// an object array where new events can be logged in JS
+const state = {
+  events: [],
+};
+
+// DOM connecting JS to the UI events list
+const eventList = document.querySelector("#event-list");
+
+// DOM connecting JS to the UI addEvent form
+const addEventForm = document.querySelector("#addEventForm");
+// event listener to run the addEvent function when the form's submit button is clicked
+addEventForm.addEventListener("submit", addEvent);
+
+async function render() {
+  await getEvents();
+  renderEvents();
+}
+render();
+
+// creating function to get from API to JS
+async function getEvents() {
+  try {
+    const response = await fetch(API_URL); // accessing API_URL
+    const json = await response.json(); // formatting fetched data to json format
+    const events = json.data; // collecting specifically the event data from the fetched url
+    state.events = events; // replacing the object array with what's in json's data.
+    // state.events = json.data;  -->  (ALTERNATIVE for the 2 lines above)
+    console.log("state: ", state); // to confirm in console what the state.events array looks like.
+  } catch (error) {
+    console.error(error); // console that an error occured if the "try" didn't work
+  }
+}
+
+// creating a function to render events from JS to UI
+function renderEvents() {
+  if (!state.events.length) {
+    // if event array is blank, end funciton.
+    return;
+  }
+
+  const eventCards = state.events.map((event) => {
+    // mapping through events array and creating a card for each event
+    const newButton = document.createElement("button"); // create a button
+    newButton.textContent = "close"; //... with an X in it...
+    newButton.addEventListener("click", deleteEvent); // .. with an event listener for a click, which will run deleteEvent fx...
+    newButton.id = event.id; // using the ID to identify what event will close.
+    const li = document.createElement("li"); // create a list
+    li.innerHTML = ` 
+        <h2>${event.name}</h2> 
+        <p>${event.description}</p>
+        <p>${new Date(event.date).toLocaleString("en-US")}/<p>
+        <p>${event.location}</p>
+        `; // format each listed event as above.
+    li.appendChild(newButton); // append the new button to the list
+    return li;
+  });
+  eventList.replaceChildren(...eventCards); // replace ul list's children with event cards
+}
+
+// creating a function to add the event to the API
+async function addEvent(event) {
+  event.preventDefault(); // prevent from being refreshed
+  try {
+    const response = await fetch(API_URL, {
+      // accessing API_URL
+      method: "POST", // POST will send data back to server/API_URL
+      headers: { "Content-Type": "application/json" }, // specifies type of content
+      body: JSON.stringify({
+        //converts to a string and readable for json
+        name: addEventForm.name.value,
+        description: addEventForm.description.value,
+        date: new Date(addEventForm.date.value).toISOString(), // converts to iso string format
+        location: addEventForm.location.value,
+      }),
+    });
+    if (!response.ok) {
+      // if response is NOT okay, throw error below
+      throw new Error("Failed to create event");
+    }
+    render();
+  } catch (error) {
+    // catch error
+    console.error(error);
+  }
+  addEventForm.reset();
+}
+
+// creating a function to delete an event
+async function deleteEvent(event) {
+  console.log("Delete Event"); // to check that function is working
+  const id = event.target.id;
+  const element = event.target; //
+  element.remove();
+  if (id != null) {
+    // if id is not null...
+    const DELETE_EVENT = `${API_URL}/${id}`; // delete event with that ID
+    try {
+      await fetch(DELETE_EVENT, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  render();
+}
